11cfd364e99576fce3171a2b177a1e61c64457fd
Joran Quinten <joranquinten@hotmail.com>
2018-04-05 20:14:47 +0200

    first iteration combining gpio & socket

--- start.py
+++ start.py
@@ -3,6 +3,22 @@ from subprocess import call
 from flask import Flask, render_template, jsonify
 from flask_socketio import SocketIO, join_room, emit
 
+# Assumes default wiring scheme
+#
+# https://raspberrytips.nl/lichtsensor-aansluiten-raspberry-pi/
+# https://pinout.xyz/
+# https://sourceforge.net/p/raspberry-gpio-python/wiki/
+#
+# Sensor      RasPi Pin   RasPi function
+# GND         6           GND
+# VCC         1           3.3v
+# SIGNAL D0   7           GPIO 4
+
+import RPi.GPIO as GPIO
+import time
+
+
+# Fileloader & Mediaplayer class
 import file_loader
 from player import Mediaplayer
 
@@ -13,6 +29,13 @@ read_folder = "./input/"
 app = Flask(__name__)
 socketio = SocketIO(app)
 media_player = Mediaplayer()
+
+# Set the connected pin function
+PIN = 4
+DEBUG = False
+
+GPIO.setmode(GPIO.BCM)   # Set up to use GPIO numbering
+GPIO.setup(PIN, GPIO.IN)  # We are reading INput, not OUTput
 
 
 @app.route('/')
@@ -70,7 +93,42 @@ def emit_player_status(status):
     emit("status", status_json)
 
 
+def waitForLight():
+    while True:
+        if GPIO.input(PIN) == False:
+            print("Light input detected")
+
+            emit("notify", "Starting playback")
+            print("Starting playback")
+            file_to_play = file_loader.random_file(read_folder)
+            emit("notify", "Loaded: " + file_to_play)
+            media_player.player_load(file_to_play, 0.5)
+            media_player.player_start()
+            emit_player_status(media_player.status())
+
+            waitForDarkness()
+            return
+        time.sleep(1)
+
+
+def waitForDarkness():
+    while True:
+        if GPIO.input(PIN):
+            print("Darkness")
+
+            emit("notify", "Stopping playback")
+            print("Stopping playback")
+            media_player.player_stop()
+            emit_player_status(media_player.status())
+
+            waitForLight()
+            return
+        time.sleep(1)
+
+
 # Execute the script
 if __name__ == '__main__':
     """ Run the socket server on the published IP address """
     socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+
+    waitForLight()
